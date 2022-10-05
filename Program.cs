e5d2b2f3147b269ed0eb408e40d0bc779a8cdf0e
Elena <metelka2015@gmail.com>
2022-10-05 13:57:29 +0500

    Изменения в задаче

--- Program.cs
+++ Program.cs
@@ -2,3 +2,37 @@
 // формирует массив из строк, длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры либо задать на старте выполнения алгоритма.  
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. 
+
+
+string[] array = new string[5] { "hello", "dog", "array", "3", ";)" };
+string[] newArray = new string[array.Length];
+
+void CreateNewArray(string[] array, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    
+    Console.WriteLine();
+}
+
+Console.WriteLine("Первоначальный массив: ");
+PrintArray(array);
+CreateNewArray(array, newArray);
+Console.WriteLine("Массив из строк, длина которых меньше либо равна 3 символа: ");
+PrintArray(newArray);
+
